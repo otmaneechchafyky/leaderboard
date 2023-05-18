57afc9d6354e9281e3b135a508d004164b5968ee
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-05-18 22:49:41 +0100

    Adding getScores and setScores files

--- src/modules/renderScores.js
+++ src/modules/renderScores.js
@@ -2,7 +2,7 @@ const renderScores = (arr, container) => {
   arr.forEach((elem) => {
     const scoreItem = document.createElement('li');
     scoreItem.innerHTML = `
-        <span>${elem.name} :</span><span class="score-showed">${elem.score}</span>
+        <span>${elem.user} :</span><span class="score-showed">${elem.score}</span>
       `;
     scoreItem.classList.add('score-item');
     container.appendChild(scoreItem);
